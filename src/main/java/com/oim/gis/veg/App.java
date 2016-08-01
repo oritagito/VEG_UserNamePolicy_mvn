5452348d3ddc6bb1b890f6d607ac3b0ffbb19d20
Vasilev-e <vasilev-e@gaz-is.ru>
2016-08-01 13:46:26 +0300

    UFA UserNamePolicy - 2016.08.01            1. Add Date            2. Update description

---
+++ src/main/java/com/oim/gis/veg/App.java
@@ -0,0 +1,10 @@
+package com.oim.gis.veg;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from VEG_UserNamePolicy 2016.08.01.");
+    }
+}
